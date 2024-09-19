e7bd85b6fc2274b70d4942fe17fffa8f14043661
juliana <julianabetancur320@gmail.com>
2024-09-18 20:41:27 -0500

    pag login,sign,home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/components/footer/footer.component';
+import { HeaderComponent } from './layout/components/header/header.component';
+import { LoginComponent } from './auth/pages/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { SignUpComponent } from './auth/pages/sign-up/sign-up.component';
+import { HomeComponent } from './auth/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component:LoginComponent},
+    {path: 'sign-up', component: SignUpComponent},
+    {path: 'home', component: HomeComponent},
+    {path: '', redirectTo: 'login', pathMatch:  'full'},
+    {path: '**', redirectTo: 'login', pathMatch:  'full'}
+    
+    
+    
+
+];
